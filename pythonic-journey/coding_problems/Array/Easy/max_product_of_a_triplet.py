eaf2f9482b8793c3c29335106605f7649211bc2b
shilpashree-ramanji <shilpa.ramanji.99@gmail.com>
2026-04-10 23:59:59 +0530

    changes

---
+++ pythonic-journey/coding_problems/Array/Easy/max_product_of_a_triplet.py
@@ -0,0 +1,15 @@
+'''
+Given an integer array, find a maximum product of a triplet in the array.
+
+Examples: 
+
+Input:  arr[ ] = [10, 3, 5, 6, 20]
+Output: 1200
+Explanation: Multiplication of 10, 6 and 20
+
+Input:  arr[ ] =  [-10, -3, -5, -6, -20]
+
+
+
+'''
+
