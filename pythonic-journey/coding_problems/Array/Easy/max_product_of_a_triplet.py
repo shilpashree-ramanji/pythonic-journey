2265734fb271fb292d2db82f6fb977a296936100
shilpashree-ramanji <shilpa.ramanji.99@gmail.com>
2026-04-12 22:02:25 +0530

    mx prodcut code

--- pythonic-journey/coding_problems/Array/Easy/max_product_of_a_triplet.py
+++ pythonic-journey/coding_problems/Array/Easy/max_product_of_a_triplet.py
@@ -13,3 +13,24 @@ Input:  arr[ ] =  [-10, -3, -5, -6, -20]
 
 '''
 
+# Python program to find a maximum product of a
+# triplet in array of integers using nestd loops
+
+# Function to find a maximum product of a triplet
+# in array of integers of size n
+def maxProduct(arr):
+    n = len(arr)
+
+    maxProduct = -10**9
+
+    for i in range(n - 2):
+        for j in range(i + 1, n - 1):
+            for k in range(j + 1, n):
+                maxProduct = max(maxProduct, arr[i] * arr[j] * arr[k])
+
+    return maxProduct
+
+
+if __name__ == "__main__":
+    arr = [10, 3, 5, 6, 20]
+    print(maxProduct(arr))
