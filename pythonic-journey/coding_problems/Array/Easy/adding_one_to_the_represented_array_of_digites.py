4045cacacc5f8eabbe32d51325a379f20f57b76f
shilpashree-ramanji <shilpa.ramanji.99@gmail.com>
2026-04-16 23:29:16 +0530

    adding changes

--- pythonic-journey/coding_problems/Array/Easy/adding_one_to_the_represented_array_of_digites.py
+++ pythonic-journey/coding_problems/Array/Easy/adding_one_to_the_represented_array_of_digites.py
@@ -17,3 +17,24 @@ Explanation: 999 + 1 = 1000
 # Python program to add 1 to a
 # number represented as an array
 
+def addOne(arr):
+
+  
+    carry = 1
+
+    for i in range(len(arr) - 1, -1, -1):
+        sum = arr[i] + carry
+        arr[i] = sum % 10
+        carry = sum // 10
+
+    if carry:
+        arr.insert(0, carry)
+
+    return arr
+
+# Driver code
+if __name__ == "__main__":
+    arr = [9, 9, 9]
+    res = addOne(arr)
+    for i in res:
+        print(i, end="")
