3833d51a0d4132d253d22852e30c68f4cf265f86
shilpashree-ramanji <shilpa.ramanji.99@gmail.com>
2026-04-20 01:11:03 +0530

    adding file

--- pythonic-journey/coding_problems/Array/Easy/Missing_and_Repeating_num_in_array.py
+++ pythonic-journey/coding_problems/Array/Easy/Missing_and_Repeating_num_in_array.py
@@ -12,3 +12,28 @@ Output: [1, 5]
 Explanation: 1 is occurs twice and 5 is missing.
 '''
 
+def findTwoElement(arr):
+    n = len(arr)
+
+    # frequency array to count occurrences
+    freq = [0] * (n + 1)
+    repeating = -1
+    missing = -1
+
+    # count frequency of each element
+    for num in arr:
+        freq[num] += 1
+
+    # identify missing and repeating numbers
+    for i in range(1, n + 1):
+        if freq[i] == 0:
+            missing = i
+        elif freq[i] == 2:
+            repeating = i
+
+    return [repeating, missing]
+
+if __name__ == "__main__":
+    arr = [3, 1, 3]
+    ans = findTwoElement(arr)
+    print(ans[0], ans[1])
