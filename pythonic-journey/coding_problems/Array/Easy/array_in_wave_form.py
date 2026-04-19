d70b37f660c8d7cd6169f07ad4f185ec38b05213
shilpashree-ramanji <shilpa.ramanji.99@gmail.com>
2026-04-19 23:21:56 +0530

    adding files

--- pythonic-journey/coding_problems/Array/Easy/array_in_wave_form.py
+++ pythonic-journey/coding_problems/Array/Easy/array_in_wave_form.py
@@ -1,18 +1,9 @@
-def sortInWave(arr):
-    
-    n = len(arr)
-   
-    # swap adjacent elements
-    for i in range(0,n-1,2):
-        arr[i], arr[i+1] = arr[i+1], arr[i]
 
-if __name__ == "__main__":
-    arr = [1, 2, 3, 4, 5]
-    sortInWave(arr)
-    for i in range(0,len(arr)):
-        print (arr[i],end=" ")
+arr = [30, 23, 16, 79, 20, 15, 5]
 
-    arr = [2,4,6,8,10,12,14,16]
-    sortInWave(arr)
-    for i in range(0,len(arr)):
-        print (arr[i],end=" ")
+arr.sort()
+
+for i in range(0, len(arr)-1, 2):
+    arr[i], arr[i+1] = arr[i+1], arr[i]
+
+print(arr)
